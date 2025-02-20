67bf9126d65c6cfc16db0376b87cbb1f850041b1
skSadi12 <sksadi433@gmail.com>
2025-02-20 06:17:38 +0600

    edit and delete butoon style done

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ function addInputs(){
     span.textContent=inputText
 
     let div = document.createElement("div")
+    div.classList.add("flex","gap-3")
 
     let editBtn = document.createElement("button")
     editBtn.textContent="Edit"
     editBtn.addEventListener("click",()=> editText(span))
-
+    editBtn.classList.add("bg-green-500", "px-1" ,"rounded-lg" ,"px-3" ,"text-white")
 
     let deleteBtn = document.createElement("button")
     deleteBtn.textContent ="Delete"
     deleteBtn.addEventListener("click",()=> li.remove())
-
+    deleteBtn.classList.add("bg-red-500", "px-1" ,"rounded-lg" ,"px-3" ,"text-white")
 
     div.appendChild(deleteBtn)
     div.appendChild(editBtn)
